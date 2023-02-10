16ae1e0f3de9afcf17ab70948ae7386d73ae62cb
Arkapravo-Ghosh <ghosharkapravo1612005@gmail.com>
2023-02-10 11:47:00 +0530

    Added Arduino Code

---
+++ src/arduino/main/main.ino
@@ -0,0 +1,32 @@
+#include <Arduino.h>
+int  obstaclePin = A1;        // This is our input pin
+void setup() {
+  pinMode(obstaclePin, INPUT);
+  Serial.begin(9600);  
+}
+void loop() {
+  int hasObstacle = analogRead(obstaclePin);
+  //Serial.println(hasObstacle);
+  if (hasObstacle < 50) {
+    Serial.print("Obstacle Detected! ");
+  }
+  else {
+    Serial.print("Path is clear! ");
+  }
+  Serial.println(hasObstacle);
+  delay(500);
+  /*
+  if (hasObstacle > )
+ 
+  {
+    Serial.print( "Stop something is ahead!!  ");
+    Serial.println(hasObstacle);
+  }
+  else
+  {
+    Serial.print("Path is clear  ");
+    Serial.println(hasObstacle);
+  }
+  */
+  //delay(100);
+}
