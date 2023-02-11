d830f95633eb4dedbb305570e708709dfda75b15
Arkapravo-Ghosh <ghosharkapravo1612005@gmail.com>
2023-02-11 15:00:21 +0530

    Minor Changes

--- src/client/main.py
+++ src/client/main.py
@@ -1,6 +1,7 @@
 #!/bin/python3
 import mariadb as connector
-import time
+#import time
+#import os
 
 # MariaDB configuration
 sqlhost = "Ark-Pi"
@@ -23,15 +24,20 @@ cursor = cnx.cursor()
 def execute(arg):
     cursor.execute(arg)
 
+#def os_clear():
+#    if os.name == "nt":
+#        os.system("cls")
+#    else:
+#        os.system("clear")
 
 def main():
     query = "SELECT * FROM main"
     execute(query)
     result = cursor.fetchall()
-    print("-" * 31)
+    print("+" + "-" * 18 + "+" + "-" * 10 + "+")
     print("|" + " Parking Slot No." + " |", end="")
     print(" Status" + "   |")
-    print("-" * 31)
+    print("+" + "-" * 18 + "+" + "-" * 10 + "+")
     print("| 1                |", end="")
     if result[0][1] == 1:
         print(" " + "Occupied" + " |")
@@ -42,8 +48,7 @@ def main():
         print(" " + "Occupied" + " |")
     else:
         print(" " + "Free" + "     |")
-    print("-" * 31)
-    time.sleep(0.1)
+    print("+" + "-" * 18 + "+" + "-" * 10 + "+")
 
 
 if __name__ == "__main__":
